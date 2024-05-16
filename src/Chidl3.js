a59c228e81c75795408f3250c3e0cb7d62e72c31
Yogesh Gupta <yogesh.gupta@amla.io>
2024-05-16 15:00:26 +0530

    scu-222: my commit changesw

---
+++ src/Chidl3.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Chidl3() {
+  return <div>Chidl3</div>;
+}
---
+++ src/Child4.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Chidl4() {
+  return <div>Chidl4</div>;
+}
