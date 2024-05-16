6d966d48c411f3fef5aab91100a435b048244095
Yogesh Gupta <yogesh.gupta@amla.io>
2024-05-16 14:44:42 +0530

    scu-222: mychanges

---
+++ src/Demo.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ChildOne from "./ChildOne";
+
+export default function Demo() {
+  return (
+    <div>
+      Demo <small>1</small>
+      <ChildOne />
+      <div>hello</div>
+    </div>
+  );
+}
